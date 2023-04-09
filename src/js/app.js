5caed40710345e4482f9f343827573e1665cd3fe
Taisiia Zhdanovych <taisiya.zhdan@gmail.com>
2023-04-09 21:58:01 +0300

    connect render and search functions

--- src/js/app.js
+++ src/js/app.js
@@ -2,10 +2,15 @@
 
 // functionSearch();
 
+//  import * as functionSearch from "./modules/search.js";
 
-import { render } from "./modules/script.js"
+// import { render } from "./modules/script.js";
+// import * as functionSearch from "./modules/search.js";
+// import * as functionSearch from "./modules/search.js";
 
-
-import * as login from "./modules/_login.js"
-
+// import * as login from "./modules/_login.js";
+// import * as functionSearch from "./modules/search.js";
+import { render } from "./modules/script.js";
 import * as functionSearch from "./modules/search.js";
+import * as login from "./modules/_login.js";
+// import * as functionSearch from "./modules/search.js";
