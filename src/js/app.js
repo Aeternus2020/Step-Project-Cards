0b0716208881715d4d4bebbae4a682ee3dfae457
Olena Hodyna <crimeaodessa2006@gmail.com>
2023-04-11 16:59:10 +0200

    delete errors in console

--- src/js/app.js
+++ src/js/app.js
@@ -19,6 +19,8 @@ import * as create from "./modules/create.js";
 import { del } from "./modules/delete.js";
 
 import * as functionSearch from "./modules/search.js";
+import * as functionSearch from "./modules/search.js";
+import * as login from "./modules/login.js";
 // import * as functionSearch from "./modules/search.js";
 
 
