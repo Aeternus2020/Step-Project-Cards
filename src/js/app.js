2dce357b477aa052ca50d8fcd0b674b500e509d0
Taisiia Zhdanovych <taisiya.zhdan@gmail.com>
2023-04-12 20:44:06 +0300

    accept current changes

--- src/js/app.js
+++ src/js/app.js
@@ -1,17 +1,8 @@
 
 // functionSearch();
-<<<<<<< HEAD
-
-
-=======
->>>>>>> 91c93fba05acbf6a626b3e973aa5419ecf424c77
 import { render } from "./modules/script.js";
 import * as create from "./modules/create.js";
 import { del } from "./modules/delete.js";
 import * as functionSearch from "./modules/search.js";
-<<<<<<< HEAD
-import * as login from "./modules/_login.js";
-=======
 import * as login from "./modules/login.js";
 
->>>>>>> 91c93fba05acbf6a626b3e973aa5419ecf424c77
