0fbc71bafe11c3a85f4602335b093402f607f087
Taisiia Zhdanovych <taisiya.zhdan@gmail.com>
2023-04-12 19:53:15 +0300

    delete comments

--- src/js/app.js
+++ src/js/app.js
@@ -2,15 +2,7 @@
 
 // functionSearch();
 
-//  import * as functionSearch from "./modules/search.js";
 
-// import { render } from "./modules/script.js";
-// import * as functionSearch from "./modules/search.js";
-// import * as functionSearch from "./modules/search.js";
-
-// import * as login from "./modules/_login.js";
-// import * as functionSearch from "./modules/search.js";
 import { render } from "./modules/script.js";
 import * as functionSearch from "./modules/search.js";
 import * as login from "./modules/_login.js";
-// import * as functionSearch from "./modules/search.js";
